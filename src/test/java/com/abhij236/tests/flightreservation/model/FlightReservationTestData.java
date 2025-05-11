b3628c37d4ca98ad3a2bbd4dc7401187f97d4508
Abhij-Test236 <abhij9231643891@gmail.com>
2025-05-11 13:37:39 +0530

    adding selenium project with grid and docker compose

---
+++ src/test/java/com/abhij236/tests/flightreservation/model/FlightReservationTestData.java
@@ -0,0 +1,12 @@
+package com.abhij236.tests.flightreservation.model;
+
+public record FlightReservationTestData(String firstName,
+                                        String lastName,
+                                        String email,
+                                        String password,
+                                        String street,
+                                        String city,
+                                        String zip,
+                                        String passengersCount,
+                                        String expectedPrice) {
+}
